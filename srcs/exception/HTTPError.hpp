d426791055792bf061b26fa5093a47492b3a5561
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-07-04 23:20:19 +0900

    e_statusを追加。エラーになるconstを外しました。

--- srcs/exception/HTTPError.hpp
+++ srcs/exception/HTTPError.hpp
@@ -6,9 +6,16 @@
 class HTTPError : public std::exception
 {
 	public:
+		enum	e_status
+		{
+			BAD_REQUEST = 400,
+			FORBIDDEN = 403,
+			NOT_FOUND = 404,
+		};
+
 		explicit HTTPError(const int status_code) : status_code_(status_code) {}
 
-		const int	GetStatusCode() const { return (status_code_); }
+		int	GetStatusCode() const { return (status_code_); }
 
 	private:
 		int		status_code_;
