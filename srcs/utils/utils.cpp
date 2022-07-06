5795c39234648c02a60cee5b01614df1c268848a
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-07-06 13:58:38 +0900

    utilsにmy_splitを追加しました。

---
+++ srcs/utils/utils.cpp
@@ -0,0 +1,34 @@
+#include "utils.hpp"
+
+std::vector<std::string>	my_split
+	(std::string const & str, std::string const & separator)
+{
+	std::vector<std::string>	list;
+	std::string::size_type		sep_len;
+	std::string::size_type		offset;
+	std::string::size_type		pos;
+
+	sep_len = separator.length();
+
+	if (sep_len == 0)
+		list.push_back(str);
+	else
+	{
+		offset = 0;
+		while (1)
+		{
+			pos = str.find(separator, offset);
+			if (pos == std::string::npos)
+			{
+				list.push_back(str.substr(offset));
+				break;
+			}
+			list.push_back(str.substr(offset, pos - offset));
+			offset = pos + sep_len;
+			while (str.substr(offset, sep_len) == separator)
+				offset += sep_len;
+		}
+	}
+
+	return (list);
+}
---
+++ srcs/utils/utils.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILS_HPP
+# define UTILS_HPP
+
+# include <string>
+# include <vector>
+
+std::vector<std::string>	my_split
+	(std::string const & str, std::string const & separator);
+
+
+#endif
