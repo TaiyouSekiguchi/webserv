f162755e810cbbef08303c55766e29d634d3fc7e
mkamei <mkamei@student.42tokyo.jp>
2022-07-14 17:49:37 +0900

    POST のテストを追加

---
+++ tests/unit_tests/HTTPMethod_test/testsrcs/POST_test.cpp
@@ -0,0 +1,86 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include "ListenSocket.hpp"
+#include "ServerSocket.hpp"
+#include "ClientSocket.hpp"
+#include "Config.hpp"
+#include "HTTPRequest.hpp"
+#include "HTTPMethod.hpp"
+
+class POSTTest : public ::testing::Test
+{
+	protected:
+		static void SetUpTestCase()
+		{
+			lsocket_ = new ListenSocket(*(config_.GetServers().begin()));
+			lsocket_->ListenConnection();
+			csocket_ = new ClientSocket();
+			csocket_->ConnectServer("127.0.0.1", 8080);
+			ssocket_ = new ServerSocket(lsocket_->AcceptConnection(), lsocket_->GetServerConf());
+		}
+    	static void TearDownTestCase()
+		{
+			delete lsocket_;
+			delete ssocket_;
+			delete csocket_;
+		}
+
+		void	RunCommunication(const std::string& msg)
+		{
+			try
+			{
+				csocket_->SendRequest(msg);
+				req_.ParseRequest(*ssocket_, ssocket_->GetServerConf());
+				status_code_ = method_.ExecHTTPMethod(req_, ssocket_->GetServerConf());
+			}
+			catch (const HTTPError& e)
+			{
+				status_code_ = e.GetStatusCode();
+			}
+		}
+
+		static Config			config_;
+		static ListenSocket		*lsocket_;
+		static ServerSocket 	*ssocket_;
+		static ClientSocket		*csocket_;
+
+		int						status_code_;
+		HTTPRequest				req_;
+		HTTPMethod				method_;
+};
+
+Config			POSTTest::config_("conf/post.conf");
+ListenSocket*	POSTTest::lsocket_ = NULL;
+ServerSocket*	POSTTest::ssocket_ = NULL;
+ClientSocket*	POSTTest::csocket_ = NULL;
+
+TEST_F(POSTTest, NotAllowedTest)
+{
+	RunCommunication("POST / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	EXPECT_EQ(status_code_, HTTPError::METHOD_NOT_ALLOWED);
+}
+
+TEST_F(POSTTest, NotFoundTest)
+{
+	RunCommunication("POST /upload/no HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	EXPECT_EQ(status_code_, HTTPError::NOT_FOUND);
+}
+
+TEST_F(POSTTest, NotDirTest)
+{
+	RunCommunication("POST /upload/index.html HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	EXPECT_EQ(status_code_, HTTPError::CONFLICT);
+}
+
+TEST_F(POSTTest, UploadTest)
+{
+	RunCommunication("POST /upload HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	EXPECT_EQ(status_code_, 201);
+	EXPECT_NE(method_.GetLocation(), "");
+}
+
+TEST_F(POSTTest, SameFileTest)
+{
+	RunCommunication("POST /upload HTTP/1.1\r\nHost: localhost:8080\r\n\r\n");
+	EXPECT_EQ(status_code_, HTTPError::CONFLICT);
+}
