a6166794722d4d203056b5a39ded07bfe168a55a
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-07-27 14:41:30 +0900

    HTTPErrorにmsg変数をもたせました。

--- srcs/exception/HTTPError.hpp
+++ srcs/exception/HTTPError.hpp
@@ -18,12 +18,14 @@ class HTTPError : public std::exception
 			HTTP_VERSION_NOT_SUPPORTED = 505
 		};
 
-		explicit HTTPError(const int status_code) : status_code_(status_code) {}
+		explicit HTTPError(const int status_code, const std::string& msg = "HTTPError") : status_code_(status_code), msg_(msg) {}
 
-		int	GetStatusCode() const { return (status_code_); }
+		int				GetStatusCode() const { return (status_code_); }
+		std::string		GetMsg() const { return (msg_ + " throw exception."); }
 
 	private:
-		int		status_code_;
+		int				status_code_;
+		std::string		msg_;
 };
 
 #endif
