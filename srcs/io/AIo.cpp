7bbd541c407fad578c161b40cc34f7e7915cca2c
Masaya Kamei <km9smc6@gmail.com>
2022-08-08 13:46:45 +0900

    すべてのfd をNon Blocking にした。

--- srcs/io/AIo.cpp
+++ srcs/io/AIo.cpp
@@ -1,9 +1,15 @@
 #include <unistd.h>
+#include <fcntl.h>
 #include "AIo.hpp"
 
 AIo::AIo(const int fd)
 	: fd_(fd)
 {
+	if (fd_ == -1)
+	{
+		int val = fcntl(sockfd, F_GETFL, 0);
+		fcntl(sockfd, F_SETFL, val | O_NONBLOCK);
+	}
 }
 
 AIo::~AIo()
